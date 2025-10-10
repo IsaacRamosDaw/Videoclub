08c28353f3ab21746463b6ae9049fe95cb86a762
Isaac <isaacantonioramosperez@alumno.ieselrincon.es>
2025-10-10 17:44:32 +0200

    Maven actualizado con conector

--- Videoclub/src/main/java/com/mycompany/videoclub/DAO/ClientesImpl.java
+++ Videoclub/src/main/java/com/mycompany/videoclub/DAO/ClientesImpl.java
@@ -4,10 +4,43 @@
  */
 package com.mycompany.videoclub.DAO;
 
+import com.mycompany.videoclub.DAO.Interfaces.IClientes;
+import com.mycompany.videoclub.Modelos.Clientes;
+
 /**
  *
  * @author isaac
  */
-public class ClientesImpl {
+public class ClientesImpl implements IClientes {
+
+  @Override
+  public boolean agregarCliente(Clientes u) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'agregarCliente'");
+  }
+
+  @Override
+  public String eliminarCliente(Integer id) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'eliminarCliente'");
+  }
+
+  @Override
+  public boolean actualizarCliente(Integer id, Clientes u) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'actualizarCliente'");
+  }
+
+  @Override
+  public Clientes listarClientes(String username) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'listarClientes'");
+  }
+
+  @Override
+  public boolean validarCliente(String username) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'validarCliente'");
+  }
     
 }
